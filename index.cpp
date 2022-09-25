b70d764266e7089831e852ddfddfdab994e4bf09
Yevhenii Grytsay <60937148+sultan2131@users.noreply.github.com>
2022-09-25 21:15:31 +0300

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    cout << "first number:" << endl;
+    cin >> x;
+    cout << "second number:" << endl;
+    cin >> y;
+    cout << x << "+" << y << "=" << x+y << endl;
+    return 0;
+}
